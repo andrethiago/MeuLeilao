56b0b6009f5f30c8f626daf42629c4f17b3199f8
andrethiago <andrethiago@devel>
2017-07-05 00:42:52 -0300

    Teste ajustado para uso dos page objects.

--- src/ui-test/java/br/mp/mpf/leilao/uitests/listagem/ListagemLeiloesAbertosSeleniumSuiteTest.java
+++ src/ui-test/java/br/mp/mpf/leilao/uitests/listagem/ListagemLeiloesAbertosSeleniumSuiteTest.java
@@ -1,15 +1,15 @@
 package br.mp.mpf.leilao.uitests.listagem;
 
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.not;
-import static org.junit.Assert.assertThat;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import br.mp.mpf.leilao.uitests.login.LoginPage;
 
 public class ListagemLeiloesAbertosSeleniumSuiteTest {
 
@@ -18,7 +18,7 @@ public class ListagemLeiloesAbertosSeleniumSuiteTest {
 	@Before
 	public void criaDriver() {
 		driver = new ChromeDriver();
-		//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 	@After
@@ -28,13 +28,18 @@ public class ListagemLeiloesAbertosSeleniumSuiteTest {
 
 	@Test
 	public void aposAutenticarUsuarioDeveVerListaDeTodosLeiloesAbertos() {
-		driver.get("http://localhost:8080/leilao");
+		ListagemLeilaoAbertosPage listagemLeilaoAbertosPage =
+			new LoginPage(driver).visita("http://localhost:8080/leilao").autentica("joaosilva@mpf.mp.br", "123456");
+
+		Assert.assertTrue(listagemLeilaoAbertosPage.temListaDeLeiloes());
+
+		/*driver.get("http://localhost:8080/leilao");
 		driver.findElement(By.name("username")).sendKeys("joaosilva@mpf.mp.br");
 		driver.findElement(By.name("password")).sendKeys("123456");
-
+		
 		driver.findElement(By.cssSelector("input[type='submit']")).click();
-
-		assertThat(driver.findElements(By.cssSelector("div.bs-callout")), not(empty()));
+		
+		assertThat(driver.findElements(By.cssSelector("div.bs-callout")), not(empty()));*/
 	}
 
 }
